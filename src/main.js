308896006a10e1f7e946c14db7c6d434f14b8122
sylpoio <137645121+sylpoio@users.noreply.github.com>
2023-09-12 15:08:51 -0600

    Actualización martes (#7)
    * SRM-lunes
    
    * lunes noche
    
    * martes mañana
    
    * martes 10am
    
    * se rompio
    
    * Revert "martes 10am"
    
    This reverts commit b2ab59cd67d1e7e52bc10ec0f6652ceed3bcce7a.
    
    * este es el bueno del martes
    
    * cambio letra linea 50
    
    * ajuste texto pantallas
    
    * ajuste
    
    * nuevo ajuste
    
    * el chido
    
    * otro
    
    * a ver
    
    * último que vio Sara
    
    * el bueno, aja
    
    * sin errores
    
    ---------
    
    Co-authored-by: Sara Ramirez <sara_ramatis@hotmail.com>

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import { example } from './dataFunctions.js';
+//import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 
-const prueba = document.getElementById('root');
+const prueba = document.getElementById('container');
 prueba.innerHTML = renderItems(data);
 // import data from './data/rickandmorty/rickandmorty.js';
-//console.log(example, renderItems, data);
+console.log(example, renderItems, data);
 
 
--- src/view.js
+++ src/view.js
@@ -1,22 +1,20 @@
 // estas funciones son de ejemplo
 
 export const renderItems = (data) => {
-
   const films = data.films;
-  let titlesHTML= "";
+  let titlesHTML = "";
 
   //identificacmos los datos de las peliculas
-  films.forEach(movie => {
-    titlesHTML+=`<ul>
-    <li>${movie.title}</li>
-    <img src=${movie.poster}/>
-    <span>${movie.description}</span>
-    </ul>`
+  films.forEach((movie) => {
+    titlesHTML += `
+    <li>
+    <img src=${movie.poster} alt="${movie.title} Poster"/>
+    <h3>${movie.title}</h3>
+    </li>`;
   });
 
+  return titlesHTML;
 };
 
-
- return titlesHTML
-  
-};
+//<h3>${movie.title}</h3>
+//<p>${movie.description}</p>
