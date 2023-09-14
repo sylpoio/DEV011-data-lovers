085b8ebc2d4e77fdd4a69312ae2ad60bccb71576
sylpoio <syl.gomez.bio@gmail.com>
2023-09-14 15:51:16 -0600

    hover-intento1

--- src/view.js
+++ src/view.js
@@ -2,7 +2,7 @@
 
 export const renderItems = (data) => {
   const films = data.films;
-  let titlesHTML  = "";
+  let titlesHTML = "";
 
   //identificacmos los datos de las peliculas
   films.forEach((movie) => {
@@ -10,13 +10,19 @@ export const renderItems = (data) => {
     <li class="card">
       <div class="front">
         <img src=${movie.poster} alt="${movie.title} Poster"/>
-        
-      </div>
-      <div class="back">
-        <div class="title"></div>
-        <div class="poster"></div>
-        <div class="decription"></div>
-        <div class="back-card2"></div>
+        </div>
+        <div class= overlay>  
+          <div class="back">
+            <div class="decription">
+              <h3>${movie.title}</h3>
+              <p>${movie.description}</p>
+            </div>  
+          </div>  
+          <div class="back-card2">
+            <div class="title"></div>
+            <div class="poster"></div>
+          </div>
+        </div>
       </div>
     </li> `;
   });
