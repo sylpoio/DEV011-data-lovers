848f73054cb42504089750f00450003c8eea388b
Sara Ramirez <sara_ramatis@hotmail.com>
2023-09-14 17:49:19 -0600

    SRM-jueves_6:00

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -7,3 +7,16 @@ export const example = () => {
 export const anotherExample = () => {
   return [];
 };
+
+function myFunc() {
+  const parteFront = document.getElementsByClassName('front');
+  const parteBack = document.getElementsByClassName('back');
+  
+ if (parteFront.style.visibility === 'hidden') {
+  parteFront.style.visibility = 'visible';
+} else {
+  parteFront.style.visibility = 'hidden';
+  parteBack.style.visibility = 'hidden';
+}
+
+}
--- src/view.js
+++ src/view.js
@@ -1,22 +1,40 @@
 // estas funciones son de ejemplo
 
+
 export const renderItems = (data) => {
   const films = data.films;
+  const people = data.films.people;
   let titlesHTML = "";
 
   //identificacmos los datos de las peliculas
   films.forEach((movie) => {
     titlesHTML += `
-    <li class="card">
+    <li class="card" onclick="miFunc()">
       <div class="front">
-        <img src=${movie.poster} alt="${movie.title} Poster"/>
-        <h3>${movie.title}</h3>
+        <img src=${movie.poster} alt="${movie.title} Poster" class="primer"/>
       </div>
       <div class="back">
-        <div class="title">${movie.title}</div>
-        <div class="back-card2"><img src="images/tarjeta2.jpg" alt="informacion"/></div>
-        <div class="imgSmall"><img src=${movie.poster} alt="${movie.title} Poster"/></div>
-        <div class="description">${movie.description}</div>
+      <img src="images/tarjeta2.jpg" alt="informacion" class="segunda"/>
+      <div class="title">${movie.title}</div>
+      <img src=${movie.poster} alt="${movie.title} Poster" class="imgSmall"/>
+      <table border=0 class="description">
+        <tr>
+        <td width="180px">Director:${movie.director}</td>
+        <td><a href="people.html">People: </a></td>
+        </tr>
+
+        <tr>
+        <td>Producer: ${movie.producer}</td>
+        <td><a href="">Locations: </a></td>
+        </tr>
+
+        <tr>
+        <td>Release date: ${movie.release_date}</td>
+        <td><a href="">Vehicles: </a></td>
+        </tr>
+
+        <tr><td>Score: ${movie.rt_score}</td></tr>
+      </table>
       </div>
     </li> `;
   });
