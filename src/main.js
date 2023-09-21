928f7dbf52eeb76eea965a45c492e5e41490e687
Sara Ramirez <sara_ramatis@hotmail.com>
2023-09-20 18:08:28 -0600

    SRM-miercolesSelect

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { example } from "./dataFunctions.js";
+import { selectDirector,selectProducer, selectDate } from "./dataFunctions.js";
 import { renderItems, renderItems2 } from "./view.js";
 
 // import data from './data/lol/lol.js';
@@ -6,6 +6,8 @@ import data from "./data/ghibli/ghibli.js";
 
 const indexHTML = document.querySelector("#root");
 const selecD = document.querySelector('[for="director"]');
+const selecP = document.querySelector('[for="producer"]');
+const selecR = document.querySelector('[for="release_date"]');
 const currentPageURL = window.location.href;
 
 //console.log(currentPageURL);
@@ -13,7 +15,9 @@ if (currentPageURL.includes("info")) {
   indexHTML.appendChild(renderItems2()); // Llama a la función para la página de información
 } else {
   indexHTML.appendChild(renderItems(data)); // Llama a la función para la página principal (o cualquier otra página)
-  selecD.appendChild(example(data));
+  selecD.appendChild(selectDirector(data));
+  selecP.appendChild(selectProducer(data));
+  selecR.appendChild(selectDate(data));
   //console.log(example(data));
 }
 
