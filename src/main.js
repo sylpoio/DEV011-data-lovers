1b2fdd0b6b44c555335c8b902ee9429780c12391
Sara Ramirez <sara_ramatis@hotmail.com>
2023-09-21 13:10:25 -0600

    SRM-jueves21

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import { renderItems, renderItems2 } from "./view.js";
 import data from "./data/ghibli/ghibli.js";
 
 const indexHTML = document.querySelector("#root");
-const selecD = document.querySelector('[for="director"]');
-const selecP = document.querySelector('[for="producer"]');
-const selecR = document.querySelector('[for="release_date"]');
+const menuDirector = document.querySelector('[for="director"]');
+const menuProducer = document.querySelector('[for="producer"]');
+const menuDate = document.querySelector('[for="release_date"]');
 const currentPageURL = window.location.href;
 
 //console.log(currentPageURL);
@@ -15,9 +15,9 @@ if (currentPageURL.includes("info")) {
   indexHTML.appendChild(renderItems2()); // Llama a la función para la página de información
 } else {
   indexHTML.appendChild(renderItems(data)); // Llama a la función para la página principal (o cualquier otra página)
-  selecD.appendChild(selectDirector(data));
-  selecP.appendChild(selectProducer(data));
-  selecR.appendChild(selectDate(data));
+  menuDirector.appendChild(selectDirector(data));
+  menuProducer.appendChild(selectProducer(data));
+  menuDate.appendChild(selectDate(data));
   //console.log(example(data));
 }
 
